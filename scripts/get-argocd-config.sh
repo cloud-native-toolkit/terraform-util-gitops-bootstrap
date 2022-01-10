36962a14152b21ad7fa4b49b5ec88e5d14a62fcf
Sean Sundberg <seansund@us.ibm.com>
2022-01-10 09:34:44 -0600

    Fixes missing namespace in secret get and adds logging
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/get-argocd-config.sh
+++ scripts/get-argocd-config.sh
@@ -6,12 +6,31 @@ OUTPUT_FILE="$2"
 OUTPUT_DIR=$(dirname "${OUTPUT_FILE}")
 mkdir -p "${OUTPUT_DIR}"
 
-HOST=$(kubectl get route openshift-gitops-server -n "${NAMESPACE}" -o json | "${BIN_DIR}/jq" -r '.spec.host')
+ROUTE_NAME="openshift-gitops-server"
+SECRET_NAME="openshift-gitops-cluster"
+
+if ! kubectl get route "${ROUTE_NAME}" -n "${NAMESPACE}"; then
+  echo "Unable to find route: ${NAMESPACE}/${ROUTE_NAME}"
+  exit 1
+fi
+
+echo "Getting route from ${NAMESPACE}/${ROUTE_NAME}"
+HOST=$(kubectl get route "${ROUTE_NAME}" -n "${NAMESPACE}" -o json | "${BIN_DIR}/jq" -r '.spec.host')
 USER="admin"
-PASSWORD=$(kubectl get secret openshift-gitops-cluster -o json | "${BIN_DIR}/jq" -r '.data["admin.password"]' | base64 -d)
+
+if ! kubectl get secret "${SECRET_NAME}" -n "${NAMESPACE}"; then
+  echo "Unable to find secret: ${NAMESPACE}/${SECRET_NAME}"
+  exit 1
+fi
+
+echo "Getting password from ${NAMESPACE}/${SECRET_NAME}"
+PASSWORD=$(kubectl get secret "${SECRET_NAME}" -n "${NAMESPACE}" -o json | "${BIN_DIR}/jq" -r '.data["admin.password"]' | base64 -d)
 
 echo '{}' | "${BIN_DIR}/jq" \
   --arg HOST "${HOST}" \
   --arg USER "${USER}" \
   --arg PASSWORD "${PASSWORD}" \
   '{"host": $HOST, "user": $USER, "password": $PASSWORD}' > "${OUTPUT_FILE}"
+
+echo "Found argocd config"
+cat "${OUTPUT_FILE}"
