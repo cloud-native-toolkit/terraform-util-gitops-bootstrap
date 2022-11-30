4d8020f93cb57aba15a12e1717e7d3db91199c8b
Sean Sundberg <seansund@us.ibm.com>
2022-11-30 09:01:20 -0600

    Updates test to use gitea and provider config
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage0.tf
+++ test/stages/stage0.tf
@@ -3,13 +3,29 @@ terraform {
     clis = {
       source  = "cloud-native-toolkit/clis"
     }
+    gitops = {
+      source = "cloud-native-toolkit/gitops"
+    }
   }
+}
+
+provider "gitops" {
+  bin_dir  = data.clis_check.clis.bin_dir
+
+  default_host = module.gitea.host
+  default_org  = module.gitea.org
+  default_username = module.gitea.username
+  default_token    = module.gitea.token
+  default_ca_cert  = module.gitea.ca_cert
 }
 
 provider "clis" {
   alias = "clis2"
 
   bin_dir = "${path.cwd}/test_bin_dir"
+}
+
+data clis_check clis {
 }
 
 data clis_check clis2 {
