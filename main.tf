71d3c0bbac0ed239507b253a3c764870e16c306c
Sean Sundberg <seansund@us.ibm.com>
2023-06-29 08:53:26 -0500

    Fix variable mapping for gitops_metadata resource
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource gitops_repo repo {
 
 resource gitops_metadata metadata {
   server_name = gitops_repo.repo.result_server_name
-  branch = gitops_repo.repo.result_server_name
+  branch = gitops_repo.repo.result_branch
   credentials = gitops_repo.repo.git_credentials
   config = gitops_repo.repo.gitops_config
   kube_config_path = var.cluster_config_file
