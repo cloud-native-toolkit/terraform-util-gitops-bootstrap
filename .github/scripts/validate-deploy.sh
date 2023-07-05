e28b03bc8b609337037b532822afc6352f28fdee
Sean Sundberg <seansund@us.ibm.com>
2023-07-05 16:32:35 -0500

    Fix kubectl path value in test
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -17,23 +17,22 @@ KUBESEAL_NAMESPACE=$(cat .kubeseal_namespace)
 PROJECT_NAME=$(cat .project_name)
 APP_NAME=$(cat .app_name)
 
-KUBECTL="${BIN_DIR}/kubectl"
-
-if ! ${KUBECTL} get secret "${SECRET_NAME}" -n "${KUBESEAL_NAMESPACE}" 1> /dev/null 2> /dev/null; then
+if ! kubectl get secret "${SECRET_NAME}" -n "${KUBESEAL_NAMESPACE}" 1> /dev/null 2> /dev/null; then
   echo "Unable to find secret: ${KUBESEAL_NAMESPACE}/${SECRET_NAME}"
+
   exit 1
 else
   echo "Found secret: ${KUBESEAL_NAMESPACE}/${SECRET_NAME}"
 fi
 
-if ! ${KUBECTL} get appproject "${PROJECT_NAME}" -n "${GITOPS_NAMESPACE}" 1> /dev/null 2> /dev/null; then
+if ! kubectl get appproject "${PROJECT_NAME}" -n "${GITOPS_NAMESPACE}" 1> /dev/null 2> /dev/null; then
   echo "Unable to find project: ${GITOPS_NAMESPACE}/${PROJECT_NAME}"
   exit 1
 else
   echo "Found project: ${GITOPS_NAMESPACE}/${PROJECT_NAME}"
 fi
 
-if ! ${KUBECTL} get application "${APP_NAME}" -n "${GITOPS_NAMESPACE}" 1> /dev/null 2> /dev/null; then
+if ! kubectl get application "${APP_NAME}" -n "${GITOPS_NAMESPACE}" 1> /dev/null 2> /dev/null; then
   echo "Unable to find application: ${GITOPS_NAMESPACE}/${APP_NAME}"
   exit 1
 else
